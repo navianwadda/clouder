603afc762ccd2f40596572a5dd9dd14e784970ce
navianwadda <navianwadda@outlook.com>
2026-06-11 08:53:31 +0600

    Add SaveM3UAction import to VideoClickAction.kt

--- app/src/main/java/com/lagradost/cloudstream3/actions/VideoClickAction.kt
+++ app/src/main/java/com/lagradost/cloudstream3/actions/VideoClickAction.kt
@@ -14,6 +14,7 @@ import com.lagradost.cloudstream3.MainActivity
 import com.lagradost.cloudstream3.R
 import com.lagradost.cloudstream3.actions.temp.BiglyBTPackage
 import com.lagradost.cloudstream3.actions.temp.CopyClipboardAction
+import com.lagradost.cloudstream3.actions.temp.SaveM3UAction
 import com.lagradost.cloudstream3.actions.temp.JustPlayerPackage
 import com.lagradost.cloudstream3.actions.temp.LibreTorrentPackage
 import com.lagradost.cloudstream3.actions.temp.MpvExPackage
